80c542430c0062acff79d775965425b6a75a84af
Nikolaenko94 <grot_cs5@mail.ru>
2021-03-19 23:33:31 +0300

    feat: create function rabbitsBusiness() for counting rabbits

--- hw1/farm.js
+++ hw1/farm.js
@@ -1,1 +1,20 @@
 // TASK 3 FOR HOMEWORK 1
+function rabbitsBusiness(totalRabbits,totalMonth){
+    
+    let monthArray=["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let totalRabbitsNumber=totalRabbits;
+    let totalEveryMonth =[];
+    for(let i = 0;i<totalMonth;i++){        
+        if(i+1 % 2 === 0){
+            totalRabbitsNumber= totalRabbitsNumber - (totalRabbitsNumber * 50 / 100);
+        } else if(totalRabbits > 100){
+            totalRabbitsNumber = totalRabbitsNumber - (totalRabbitsNumber * 20 / 100);
+        };
+        totalRabbitsNumber+=20;
+        totalEveryMonth.push(monthArray[i] + ` - ${Math.round(totalRabbitsNumber)} rabbits`);
+        totalRabbitsNumber = Math.round(totalRabbitsNumber);
+    }
+    
+    let returnVarriable = String(totalRabbitsNumber) + " - total rabbits" + `\n` + totalEveryMonth.join();
+    return returnVarriable;
+};
