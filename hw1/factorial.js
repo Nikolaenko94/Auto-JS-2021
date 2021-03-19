25e86ab5967205cb77644a95c2f2b47f09ea83cb
Nikolaenko94 <grot_cs5@mail.ru>
2021-03-19 15:26:46 +0300

    fix: add factorial.js, and moved function for factorial from index.js

---
+++ hw1/factorial.js
@@ -0,0 +1,14 @@
+// TASK 1 FOR HOMEWORK 1
+
+function factorial(number){
+    if(number<12 || (number ^ 0) !== number){
+       return "Enter integer greater than 12";
+    }
+    let bigNumber = BigInt(number);    
+    let factorialNumber = 1n;    
+    for(let i=0n;i<bigNumber;i++){        
+        factorialNumber *= bigNumber -i;
+    };
+    return String(factorialNumber);
+};
+
