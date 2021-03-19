118c55545abf3a60010ad4ac2adcfc6ca451ba22
Nikolaenko94 <grot_cs5@mail.ru>
2021-03-19 03:39:05 +0300

    feat: Add function factorial() for calculate factorial of numver(greater than 12); taks 1 - finished

--- hw1/index.js
+++ hw1/index.js
@@ -1,9 +1,21 @@
 // import 1 task
+factorial(28);
 // import 2 task
 // import 3 task
 
 
-
 // usage 1 task
+function factorial(number){
+    if(number<12 || (number ^ 0) !== number){
+       return console.log("Enter integer greater than 12");
+    }
+    let bigNumber = BigInt(number);    
+    let factorialNumber = 1n;    
+    for(let i=0n;i<bigNumber;i++){        
+        factorialNumber *= bigNumber -i;
+    };
+    return console.log(String(factorialNumber));
+};
+
 // usage 2 task
 // usage 3 task
