a4c190998be8132083f339e2837ff54315785791
Nikolaenko94 <grot_cs5@mail.ru>
2021-03-19 16:51:32 +0300

    feat: import and using fibonacci() function into index.js

--- hw1/index.js
+++ hw1/index.js
@@ -1,10 +1,12 @@
 // import 1 task
 const factorial = require("./factorial");
 // import 2 task
+const fibonacci = require("./fibonacci");
 // import 3 task
 
 
 // usage 1 task
 console.log(factorial(27));
 // usage 2 task
+console.log(fibonacci(100));
 // usage 3 task
