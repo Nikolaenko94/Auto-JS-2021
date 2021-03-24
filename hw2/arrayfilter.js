9fe19c058b93a3e68e52e859fb0f11f8961c2539
Nikolaenko94 <grot_cs5@mail.ru>
2021-03-24 11:22:46 +0300

    init: start task2 for homework2. Created arrayfilter.js into hw2

---
+++ hw2/arrayfilter.js
@@ -0,0 +1,44 @@
+const students = [
+    {
+        name: 'Vasya Pupkin',
+        age: 17,
+        sex: 'Male',
+        isMarried: false,
+        city: 'Mogilev',
+    },
+    {
+        name: 'Zoya Petrovna',
+        age: 23,
+        sex: 'Female',
+        isMarried: true,
+        city: 'Mogilev',
+    },
+    {
+        name: 'Petr Ivanov',
+        age: 30,
+        sex: 'Male',
+        isMarried: true,
+        city: 'Minsk',
+    },
+    {
+        name: 'Vitali Ivanov',
+        age: 19,
+        sex: 'Male',
+        isMarried: false,
+        city: 'Vitebsk',
+    },
+    {
+        name: 'Lavrenti Sakalov',
+        age: 20,
+        sex: 'Male',
+        isMarried: true,
+        city: 'Brest',
+    },
+    {
+        name: 'Olga Sakalova',
+        age: 23,
+        sex: 'Female',
+        isMarried: true,
+        city: 'Grodno',
+    },
+];
