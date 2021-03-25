b4ae35a0c339c5418465c43e99d1b332d3b429b4
Nikolaenko94 <grot_cs5@mail.ru>
2021-03-25 23:48:54 +0300

    fix: optimization function converter() for task1.

--- hw2/converter.js
+++ hw2/converter.js
@@ -1,20 +1,15 @@
 globalThis.converter=function(arr){
     let newObject={};
-    let keyBox = [];
-    let valueBox = [];
-  
-    keyBox = arr.map((element,index)=>index%2===0?element:false);
-    valueBox = arr.map((element,index)=>index%2===0?false:element);
-    keyBox.forEach((element,index)=> element === false?keyBox.splice(index,1):true);
-    valueBox.forEach((element,index)=> element === false?valueBox.splice(index,1):true);
- 
-    valueBox.length !== keyBox.length ? valueBox.push(null): true;
-    valueBox.push(keyBox.length);
-    keyBox.push("length");  
+    let counter = 0;    
+    arr.forEach((element,index) => {      
+          if(index %2 ===0){
+            newObject[arr[index]] = arr[index+1];
+            counter++;
+          }          
+    });
+
+    arr.length % 2 ===0?true:newObject[arr[arr.length-1]]= null;
+    newObject["length"] = counter;  
     
-    for(let i = 0; i < keyBox.length;i++){
-      newObject[keyBox[i]] = valueBox[i];        
-    };
-     
     return newObject;
 };
