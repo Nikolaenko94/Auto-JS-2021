30f5490ed7f4f5a3016c5ea581ad8e9745dd8a36
Nikolaenko94 <grot_cs5@mail.ru>
2021-03-27 01:35:19 +0300

    initL: start task 2 for homework 3. Created card.js file

--- hw3/index.js
+++ hw3/index.js
@@ -1,12 +1,13 @@
-// import 1 task
+// import task 1
 const multiplication = require("./multiplication");
+// import task 2
 
 
-
-// usage 1 task
-console.log("====================TASK 1 ==================")
+// usage task 1
+console.log("====================TASK 1 ==================");
 console.log(multiplication.multi(3));
 console.log(multiplication.multi(5));
 console.log(multiplication.multi(2));
-
+// usage task 2
+console.log("====================TASK 2 ==================");
 
