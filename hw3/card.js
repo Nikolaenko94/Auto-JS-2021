c8d15ee9e10ef39eeb981ff1d3ef184a6d0297d0
Nikolaenko94 <grot_cs5@mail.ru>
2021-03-27 17:04:04 +0300

    init: add card.js with function constructor Card. Export/import - done

---
+++ hw3/card.js
@@ -0,0 +1,31 @@
+function Card(name, newCardBalance){          
+    this.name = name;
+    isNaN(newCardBalance)?newCardBalance=0:true;       
+    // chekcBalance function    
+    this.checkBalance = function (){        
+        return newCardBalance;
+    };       
+    //balance replenishment function
+    this.addBalance = function (summ){
+        newCardBalance += summ;
+        return newCardBalance;
+    };
+    //write-off balance function
+    this.offBalance = function(summ){
+        if(summ > newCardBalance){return "User has not money to write-off"}       
+        else{
+            newCardBalance -= summ;
+            return newCardBalance;
+        }        
+    };
+    //transfer to another currency function
+    this.transferBalance = function(course){
+        return newCardBalance * course;
+    };    
+};
+
+let NikolaenkoCard = new Card(process.env.USERNAME,20);
+module.exports = {
+    Card,
+    NikolaenkoCard,
+};
--- hw3/index.js
+++ hw3/index.js
@@ -1,7 +1,7 @@
 // import task 1
 const multiplication = require("./multiplication");
 // import task 2
-
+const cardCreating = require("./card");
 
 // usage task 1
 console.log("====================TASK 1 ==================");
@@ -10,4 +10,14 @@ console.log(multiplication.multi(5));
 console.log(multiplication.multi(2));
 // usage task 2
 console.log("====================TASK 2 ==================");
-
+console.log(cardCreating.NikolaenkoCard.name);
+console.log(cardCreating.NikolaenkoCard.checkBalance());
+console.log(cardCreating.NikolaenkoCard.addBalance(50));
+console.log(cardCreating.NikolaenkoCard.addBalance(50));
+console.log(cardCreating.NikolaenkoCard.offBalance(200));
+console.log(cardCreating.NikolaenkoCard.offBalance(30));
+console.log(cardCreating.NikolaenkoCard.transferBalance(0.5));
+console.log(cardCreating.NikolaenkoCard.checkBalance());
+console.log("NikolaenkoCard.newCardBalance = 10000");
+cardCreating.NikolaenkoCard.newCardBalance = 10000;
+console.log(cardCreating.NikolaenkoCard.checkBalance() + " -balance not changing");
