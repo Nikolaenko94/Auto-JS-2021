e09ed061b1a284c10963de6ce2f4d5e9fbee2868
Nikolaenko94 <grot_cs5@mail.ru>
2021-03-30 18:34:40 +0300

    feat: add transferBalance() function.

--- hw4/task1/card.js
+++ hw4/task1/card.js
@@ -3,10 +3,10 @@ class Card {
     constructor(nameUser, typeOfCard){
         this.nameUser = nameUser;        
         this.typeOfCard = typeOfCard;                      
-    }
+    };
     getBalance(){
         return `Balance: ${this.#balance}`
-    }
+    };
     addBalance(summ){
         try{            
             if(summ <= 0 || isNaN(summ) || typeof summ === "boolean"){
@@ -15,9 +15,8 @@ class Card {
             this.#balance += summ;
         } catch(addBalance){
             return ("addBalance Error: " + addBalance.message);
-        };       
-                
-    }
+        };                
+    };
     offBalance(summ){  
        try{
             if(this.typeOfCard === "debit"){
@@ -30,8 +29,19 @@ class Card {
             return ("offBalance Error: " + offBalance.message);
             }; 
     };
+    transferBalance(summ){
+        try{            
+            if(summ <= 0 || isNaN(summ) || typeof summ === "boolean"){
+                throw new SyntaxError ("You entered an invalid value. Please try again.");
+            }
+            return this.#balance * summ;
+        }catch(transferBalance){
+            return ("addBalance Error: " + transferBalance.message);
+        };       
+                
+    };    
 };
-
+//EXPORT 
 module.exports = {
     Card,
 };
