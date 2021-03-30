bc7e133e2fe56e99206af7054c0c87316db42618
Nikolaenko94 <grot_cs5@mail.ru>
2021-03-30 15:45:24 +0300

    feat: created class Card with constructor with name, typeOfCard, balance

---
+++ hw4/task1/card.js
@@ -0,0 +1,12 @@
+class Card {
+    constructor(nameUser, typeOfCard, balance){
+        this.nameUser = nameUser;        
+        this.typeOfCard = typeOfCard;
+        this.balance = balance;
+    }
+
+};
+
+module.exports = {
+    Card,
+};
