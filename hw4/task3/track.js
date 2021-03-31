14e302cbe490939b1d6cec5e96ee30215bdbef3b
Nikolaenko94 <grot_cs5@mail.ru>
2021-03-31 20:53:36 +0300

    feat: created Track class into track.js

---
+++ hw4/task3/track.js
@@ -0,0 +1,13 @@
+class Track {
+    constructor(name, seconds, albom, singer, explicit){
+        this.name = name;
+        this.seconds = seconds;
+        this.albom = albom;
+        this.singer = singer;
+        this.explicit = explicit;
+    };
+};
+
+module.exports  = {
+    Track,
+}
