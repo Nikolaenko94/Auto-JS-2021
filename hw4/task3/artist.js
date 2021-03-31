35df895c8f4483ba34dd11a2ec3b3b34518b2fd1
Nikolaenko94 <grot_cs5@mail.ru>
2021-03-31 21:03:04 +0300

    feat: created Artist class into artist.js

---
+++ hw4/task3/artist.js
@@ -0,0 +1,12 @@
+class Artist {
+    constructor(name, data, ...albums, ...tracks){
+        this.name = name;
+        this.data = data;
+        this.albums = albums;
+        this.tracks = tracks;
+    };
+};
+
+module.exports = {
+    Artist,
+};
