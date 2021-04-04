cb6ccac78ffa3d832ad090a11257fc8e82cb8b04
Nikolaenko94 <grot_cs5@mail.ru>
2021-04-04 05:21:04 +0300

    feat: add event pause() for player

--- hw5/task2/library.js
+++ hw5/task2/library.js
@@ -122,7 +122,8 @@ class Library{
         const player = new EventEmitter();              
         let current = 0; 
         let myTracks = this.Track;
-        let strInfo = `${myTracks[current].name}, Duration: ${myTracks[current].seconds}`;           
+        let strInfo = `${myTracks[current].name}, Duration: ${myTracks[current].seconds}`; 
+        //EventEmitter next()        
         this.nextTrack = function(){player.once('next', ()=>{
             if(current + 1 >= myTracks.length){ 
                 current = 0;                
@@ -131,10 +132,28 @@ class Library{
                 current++;                            
             }   
                        
-        })};   
-           
+        })};
+        //EventEmitter prev()    
+        this.prevTrack = function(){player.once('prev', ()=>{
+            if(current - 1 < 0){ 
+                console.log("Invalid command");               
+            }
+            else{
+                current--;                            
+            }   
+                       
+        })};
+        //EventEmitter pause()
+        this.pauseTrack = function(){player.once('pause', ()=>{
+            mainInterval.unref();
+            clearInterval(mainInterval);
+            console.log("pause");                        
+        })};
+
         let mainInterval= setInterval(function(){
-            player.emit('next');                                   
+            player.emit('next');
+            player.emit('prev');
+            player.emit('pause');                                                        
             console.log(`${myTracks[current+1].name}, Duration: ${myTracks[current+1].seconds}`);
             current++;
             if(current + 1 >= myTracks.length){
@@ -142,10 +161,20 @@ class Library{
                 clearInterval(mainInterval);
             }                                     
         }, myTracks[current].seconds*1000);
+        //THIS CODE EMMITATION FOR WORK PAUSE AND PLAY        
+        mainInterval.ref();        
+        //END EMITATION
+        console.log("Play");
         return strInfo;
     };
     next(){
         this.nextTrack();
+    };
+    prev(){
+        this.prevTrack();
+    };
+    pause(){
+        this.pauseTrack();
     };    
 };
 module.exports = {
