73e3d60641cc48872bceb107ef474eaa3c353b45
Nikolaenko94 <grot_cs5@mail.ru>
2021-04-05 16:40:11 +0300

    fix: add check if one of arguments NaN = ErrorMessage for users

--- hw5/task1/devide.js
+++ hw5/task1/devide.js
@@ -1,6 +1,11 @@
 function devide (first,second){
     setTimeout(() => {
         console.log("devide");
+        if(isNaN(Number(first)) || isNaN(Number(first))){
+            let messageError = "Error: This line does not use a numbers";
+            console.log(messageError);
+            return messageError;
+        };
         console.log(Number(first) / Number(second));
         return  Number(first) / Number(second);
     }, 1100);
