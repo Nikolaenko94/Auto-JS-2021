7ebfba25cb142bc2ddfa9b7fc168c8442597f51d
Nikolaenko94 <grot_cs5@mail.ru>
2021-04-07 22:57:32 +0300

    feat: Task1 homework6 - done. Add method search() for library.js

--- hw6/index.js
+++ hw6/index.js
@@ -1,4 +1,20 @@
+// import task 1
+const album = require("./task1/album");
+const track = require("./task1/track");
+const artist = require("./task1/artist");
+const Library = require("./task1/library");
 // import task 2
-const loading = require('./task2/loading');
+
+//usage task 1
+let library = new Library.Library;
+let InTheEnd = new track.Track("In the End", 5, "HybridTheory", "Linkin Park", false);
+let Papercut = new track.Track("Papercut", 4, "Hybrid Theory", "Linkin Park", false);
+let HybridTheory = new album.Album("Hybrid Theory", new Date("2000-10-24"), "Linkin Park", [InTheEnd, Papercut]);
+let LinkinPark = new artist.Artist("Linkin Park", new Date("1996-01-01"), [HybridTheory],[InTheEnd, Papercut]);
+
+library.add(HybridTheory);
+library.add(LinkinPark);
+library.add(InTheEnd);
+library.add(Papercut);
+console.log(library.search("In the End"));
 //usage task 2
-loading("I replace loading symbols: ");
--- hw6/task2/loading.js
+++ hw6/task2/loading.js
@@ -1,20 +1,2 @@
 
-module.exports = function loading(params) {
-                        setInterval(() => {
-                            console.clear();
-                            console.log(`${params} |`)
-                        setTimeout(() => {
-                            console.clear();
-                            console.log(`${params} /`)
-                            setTimeout(() => {
-                                console.clear();
-                                console.log(`${params} -`)
-                                setTimeout(() => {
-                                    console.clear();
-                                    console.log(`${params} \\`)
-                                }, 300);
-                            }, 300);
-                        }, 300);
-                        }, 1200);
-};
 
