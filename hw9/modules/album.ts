6b3e71a418de7372b6931433cdf3e38de08fb136
Nikolaenko94 <grot_cs5@mail.ru>
2021-04-17 01:59:18 +0300

    feature(ФормаОсновная): created class Album & constructor for this
    1. Created into album.ts
    2.Constructer included name, dateRelease, tracks?, artist?

--- hw9/modules/album.ts
+++ hw9/modules/album.ts
@@ -1,1 +1,10 @@
 import type {MainClassesTypes} from "./_typesGuardMainClass";
+
+class Album{
+    constructor(
+        name: MainClassesTypes, 
+        dateRelease: MainClassesTypes, 
+        tracks?: MainClassesTypes, 
+        artist?: MainClassesTypes){
+    };
+};
