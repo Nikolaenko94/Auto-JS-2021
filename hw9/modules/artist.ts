7c8779259f619f1efe035bc689d0e3403862803a
Nikolaenko94 <grot_cs5@mail.ru>
2021-04-17 02:02:55 +0300

    feature(ФормаОсновная): created class Artist & constructor for this
    1.Created into artist.ts
    2.Constructor included name, dateRelease, albums?, tracks?

--- hw9/modules/artist.ts
+++ hw9/modules/artist.ts
@@ -1,1 +1,10 @@
 import type {MainClassesTypes} from "./_typesGuardMainClass";
+
+class Artist{
+    constructor(
+        name: MainClassesTypes, 
+        dateRelease: MainClassesTypes, 
+        albums?: MainClassesTypes, 
+        tracks?: MainClassesTypes){
+    };
+};
