cfd79db6d8bc15a7d8a861d97d2f5b58bd48ec53
Nikolaenko94 <grot_cs5@mail.ru>
2021-04-17 20:47:54 +0300

    build(ФормаОсновная): compilation library.ts in .js
    1. Library have constructor with track: object
    2. If we create new libarary we can add how
    arguments
    one track
    3. This track add to track storage Track: Array<object> - array with generic
    type

--- hw9/modules/library.ts
+++ hw9/modules/library.ts
@@ -1,7 +1,7 @@
 export class Library {
     //create Track for storage all tracks, using generic type
     Track: Array<object> = [];    
-    constructor(track: object ){
+    constructor(track?: object ){
         this.Track.push(track);
     }
     
---
+++ hw9/tsBuild/modules/library.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Library = void 0;
+var Library = /** @class */ (function () {
+    function Library(track) {
+        //create Track for storage all tracks, using generic type
+        this.Track = [];
+        this.Track.push(track);
+    }
+    return Library;
+}());
+exports.Library = Library;
+;
