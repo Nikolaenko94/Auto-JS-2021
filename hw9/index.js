948801688d72aabe530c0042dd045c11fc4a4a61
Nikolaenko94 <grot_cs5@mail.ru>
2021-04-17 12:48:34 +0300

    feature(ХранениеДанных): create tracks, albom, artist into index.ts
    1.Tracks: trackInTheEnd,trackPapercut,trackCastleOfGlass,trackFaint
    2.Album:
    albumHybridTheory
    3.Artist: artistLinkinPark
    4. artistLinkinPark include this Tracks and this
    Album.
    5.albumHybridTheory include this Tracks and this Artist
    6.This all Tracks include this Album
    and this Artist

--- hw9/index.js
+++ hw9/index.js
@@ -1,2 +1,22 @@
 "use strict";
 exports.__esModule = true;
+//import main classes: Track, Artist, Album;
+var track_1 = require("./modules/track");
+var album_1 = require("./modules/album");
+var artist_1 = require("./modules/artist");
+//creating tracks, albom, artist
+var trackInTheEnd = new track_1.Track("In The End", 5, false);
+var trackPapercut = new track_1.Track("Papercut", 4, false);
+var trackCastleOfGlass = new track_1.Track("Castle Of Glass", 6, false);
+var trackFaint = new track_1.Track("Faint", 4, false);
+var albumHybridTheory = new album_1.Album("Hybrid Theory", new Date("2000-10-24"), [trackInTheEnd, trackPapercut, trackCastleOfGlass, trackFaint]);
+var artistLinkinPark = new artist_1.Artist("Linkin Park", new Date("1996-01-01"), [albumHybridTheory], [trackInTheEnd, trackPapercut, trackCastleOfGlass, trackFaint]);
+albumHybridTheory.artist = artistLinkinPark;
+trackInTheEnd.album = albumHybridTheory;
+trackPapercut.album = albumHybridTheory;
+trackCastleOfGlass.album = albumHybridTheory;
+trackFaint.album = albumHybridTheory;
+trackInTheEnd.artist = artistLinkinPark;
+trackPapercut.artist = artistLinkinPark;
+trackCastleOfGlass.artist = artistLinkinPark;
+trackFaint.artist = artistLinkinPark;
--- hw9/index.ts
+++ hw9/index.ts
@@ -2,3 +2,19 @@
 import {Track} from "./modules/track"
 import {Album} from "./modules/album"
 import {Artist} from "./modules/artist"
+//creating tracks, albom, artist
+let trackInTheEnd = new Track("In The End", 5, false);
+let trackPapercut = new Track("Papercut", 4, false);
+let trackCastleOfGlass = new Track("Castle Of Glass", 6, false);
+let trackFaint = new Track("Faint", 4, false);
+let albumHybridTheory = new Album("Hybrid Theory", new Date("2000-10-24"), [trackInTheEnd, trackPapercut, trackCastleOfGlass, trackFaint]);
+let artistLinkinPark = new Artist("Linkin Park", new Date("1996-01-01"), [albumHybridTheory],[trackInTheEnd, trackPapercut, trackCastleOfGlass, trackFaint]);
+albumHybridTheory.artist = artistLinkinPark;
+trackInTheEnd.album = albumHybridTheory;
+trackPapercut.album = albumHybridTheory;
+trackCastleOfGlass.album = albumHybridTheory;
+trackFaint.album = albumHybridTheory;
+trackInTheEnd.artist = artistLinkinPark;
+trackPapercut.artist = artistLinkinPark;
+trackCastleOfGlass.artist = artistLinkinPark;
+trackFaint.artist = artistLinkinPark;
