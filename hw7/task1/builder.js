7128ba1089bf3bbd4d36cf789af7afcc568bce71
Nikolaenko94 <grot_cs5@mail.ru>
2021-04-25 00:17:07 +0300

    fix: 'any builder can append any data?'- added check for data type validity

--- hw7/task1/builder.js
+++ hw7/task1/builder.js
@@ -2,8 +2,12 @@ class Builder {
     constructor(data){
         this.data = data;
     }
-    append(value){
-        this.data += value;        
+    append(editionalData){
+        if(typeof editionalData === typeof this.data){
+            this.data += editionalData;
+        }else{
+            console.error("Invalid data type")
+        }                
     };    
     build(){
         throw new Error(`Inside the ${this.constructor.data} - Builder() not described`);
