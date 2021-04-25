6d2576187d874c31473fe391ae63f377c6c820e4
Nikolaenko94 <grot_cs5@mail.ru>
2021-04-25 23:51:39 +0300

    fix:'any builder can append the data'-fixed. Add data type check for append()

--- hw7/task1/builder.js
+++ hw7/task1/builder.js
@@ -1,8 +1,11 @@
 class Builder {
     constructor(data){
         this.data = data;
-    }
+    }       
     append(editionalData){
+        if(typeof this.data !== "string" && typeof this.data !== "number"){
+            throw new Error(`For ${typeof this.data} - append() not available`);
+        }
         if(typeof editionalData === typeof this.data){
             this.data += editionalData;
         }else{
