bf4f504a7d9c0077a12801f95c5726f0f7ef6b30
Nikolaenko94 <grot_cs5@mail.ru>
2021-05-07 15:54:15 +0300

    feat: add multiply() method for Calcalator

--- hw_test1/app/calculator.js
+++ hw_test1/app/calculator.js
@@ -10,13 +10,22 @@ class Calcalator{
             throw new Error("Invalid data type")
         };
         return a - b
-    }    
-    
+    }   
     multiply(a, b){
-
-    }
+        if(typeof a !== "number" || typeof b !== "number"){
+            throw new Error("Invalid data type")
+        };
+        if(a * b === -0){
+            return 0
+        }
+        return a * b
+    }   
     divide(a, b){
 
     }
 
 }
+
+const newCalc = new Calcalator();
+let summ = newCalc.multiply(0, -2)
+console.log(summ)
