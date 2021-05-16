2db361676e5328cd0ea4c4a99d109baa173a2fc0
Nikolaenko94 <grot_cs5@mail.ru>
2021-05-16 12:44:22 +0300

    feat: add test: should working search with key 'Enter'

--- hw_test2/test/specs/test.spec.js
+++ hw_test2/test/specs/test.spec.js
@@ -8,15 +8,18 @@ const webdriver = require("selenium-webdriver"),
       Key = webdriver.Key,
       chrome = require("selenium-webdriver/chrome"),
       path = require("chromedriver").path;
-
-const service = new chrome.ServiceBuilder(path).build();
-chrome.setDefaultService(service);
-const driver = new webdriver.Builder()
-        .withCapabilities(webdriver.Capabilities.chrome())
+       
+describe("Google.com unit testing", function(){
+    let driver;
+    beforeEach(()=>{
+        driver = new webdriver.Builder()
+        .setChromeOptions(new chrome.Options().headless().addArguments('--lang=en-EN'))
+        .withCapabilities(webdriver.Capabilities.chrome())        
         .build();
-        
-describe("Google.com unit testing", function(){                 
-         
+    });                 
+    afterEach(()=>{
+        driver = null;
+    }) 
     it('should open www.google.com',async function(){
         async function getURL(){
             await driver.get('https://www.google.com/');
@@ -27,4 +30,12 @@ describe("Google.com unit testing", function(){
         let url = await getURL();                   
         await expect(url).to.equal('https://www.google.com/');                  
     });
+    it('should working search with key "Enter"',async function(){               
+        await driver.get('https://www.google.ru/');           
+        await driver.findElement(By.name("q")).sendKeys('wikipedia', Key.ENTER);        
+        let titleAfterSearh = await driver.getTitle();
+        await driver.quit()                           
+        await expect(titleAfterSearh).to.equal('wikipedia - Google Search');                  
+    });
+
 });    
