9b8b723239fe3a0beb6c8fa834ec02cb8b00daa3
Nikolaenko94 <grot_cs5@mail.ru>
2021-05-18 22:46:34 +0300

    feat: add tet:should correctly displayed panel 'content info' for search results page

--- hw_test3/test/specs/test.spec.js
+++ hw_test3/test/specs/test.spec.js
@@ -21,76 +21,76 @@ describe("Google.com unit testing", function(){
     afterEach(()=>{
         driver = null;
     }) 
-    it('should open www.google.com',async function(){
-        async function getURL(){
-            await driver.get('https://www.google.com/');
-            let url = await driver.getCurrentUrl();
-            await driver.quit();
-            return url
-        };
-        let url = await getURL();                   
-        await expect(url).to.equal('https://www.google.com/');                  
-    });
-    it('should working search with key "Enter"',async function(){               
-        await driver.get('https://www.google.com/');           
-        await driver.findElement(By.name("q")).sendKeys('wikipedia', Key.ENTER);        
-        let titleAfterSearh = await driver.getTitle();
-        await driver.quit()                           
-        await expect(titleAfterSearh).to.equal('wikipedia - Google Search');                  
-    });
-    it('should working search with press button "Google Search"',async function(){                     
-        await driver.get('https://www.google.com/');                          
-        await driver.findElement(By.name("q")).sendKeys('wikipedia');        
-        await (await driver).sleep(500);        
-        await driver.findElement(By.name("btnK")).click();                        
-        let titleAfterSearh = await driver.getTitle();
-        await driver.quit();                           
-        await expect(titleAfterSearh).to.equal('wikipedia - Google Search');                  
-    });
-    it('should opens doodles page after click button "I"m Feeling Lucky"',async function(){                           
-        await driver.get('https://www.google.com/');                                 
-        await driver.findElement(By.css(".FPdoLc.lJ9FBc center .RNmpXc")).click();                        
-        let titleAfterSearh = await driver.getTitle();
-        await driver.quit();                           
-        await expect(titleAfterSearh).to.equal('Google Doodles');                  
-    });
-    it('should opens img-search page after click link "Images"',async function(){                           
-        await driver.get('https://www.google.com/');                                 
-        await driver.findElement(By.linkText("Images")).click();                        
-        let titleAfterClick = await driver.getTitle();
-        await driver.quit();                           
-        await expect(titleAfterClick).to.equal('Google Images');                  
-    });
-    it('Img-search page should have special text "images" with logo ',async function(){
-        let result = {
-            visibleAtPage: null,
-            text: null,
-        }
-        let finalyResult = {
-            visibleAtPage: true,
-            text: "images",
-        }                          
-        await driver.get('https://www.google.com/imghp?hl=en&ogbl');                                 
-        let displaySpan = await driver.findElement(By.css("#hplogo .a9nkvf span")).isDisplayed();
-        let textIntoLink = await driver.findElement(By.css("#hplogo .a9nkvf span")).getText();
-        await (function resultAfterCheck(){
-            result.visibleAtPage = displaySpan;
-            result.text = textIntoLink;
-        })();                               
-        await expect(result).to.eql(finalyResult);                  
-    });
-    it('should appears special window for searching by image after press button "Search by image"',async function(){                           
-        await driver.get('https://www.google.com/imghp?hl=en&ogbl');                                 
-        await driver.findElement(By.xpath("//*[@class='LM8x9c']")).click();
-        await (await driver).sleep(1000);        
-        let windowForSearchDisplayed = await driver.findElement(By.id("QDMvGf")).isDisplayed();                          
-        await driver.quit();                           
-        await expect(windowForSearchDisplayed).to.equal(true);                  
-    });
+    // it('should open www.google.com',async function(){
+    //     async function getURL(){
+    //         await driver.get('https://www.google.com/');
+    //         let url = await driver.getCurrentUrl();
+    //         await driver.quit();
+    //         return url
+    //     };
+    //     let url = await getURL();                   
+    //     await expect(url).to.equal('https://www.google.com/');                  
+    // });
+    // it('should working search with key "Enter"',async function(){               
+    //     await driver.get('https://www.google.com/');           
+    //     await driver.findElement(By.name("q")).sendKeys('wikipedia', Key.ENTER);        
+    //     let titleAfterSearh = await driver.getTitle();
+    //     await driver.quit()                           
+    //     await expect(titleAfterSearh).to.equal('wikipedia - Google Search');                  
+    // });
+    // it('should working search with press button "Google Search"',async function(){                     
+    //     await driver.get('https://www.google.com/');                          
+    //     await driver.findElement(By.name("q")).sendKeys('wikipedia');        
+    //     await (await driver).sleep(500);        
+    //     await driver.findElement(By.name("btnK")).click();                        
+    //     let titleAfterSearh = await driver.getTitle();
+    //     await driver.quit();                           
+    //     await expect(titleAfterSearh).to.equal('wikipedia - Google Search');                  
+    // });
+    // it('should opens doodles page after click button "I"m Feeling Lucky"',async function(){                           
+    //     await driver.get('https://www.google.com/');                                 
+    //     await driver.findElement(By.css(".FPdoLc.lJ9FBc center .RNmpXc")).click();                        
+    //     let titleAfterSearh = await driver.getTitle();
+    //     await driver.quit();                           
+    //     await expect(titleAfterSearh).to.equal('Google Doodles');                  
+    // });
+    // it('should opens img-search page after click link "Images"',async function(){                           
+    //     await driver.get('https://www.google.com/');                                 
+    //     await driver.findElement(By.linkText("Images")).click();                        
+    //     let titleAfterClick = await driver.getTitle();
+    //     await driver.quit();                           
+    //     await expect(titleAfterClick).to.equal('Google Images');                  
+    // });
+    // it('Img-search page should have special text "images" with logo ',async function(){
+    //     let result = {
+    //         visibleAtPage: null,
+    //         text: null,
+    //     }
+    //     let finalyResult = {
+    //         visibleAtPage: true,
+    //         text: "images",
+    //     }                          
+    //     await driver.get('https://www.google.com/imghp?hl=en&ogbl');                                 
+    //     let displaySpan = await driver.findElement(By.css("#hplogo .a9nkvf span")).isDisplayed();
+    //     let textIntoLink = await driver.findElement(By.css("#hplogo .a9nkvf span")).getText();
+    //     await (function resultAfterCheck(){
+    //         result.visibleAtPage = displaySpan;
+    //         result.text = textIntoLink;
+    //     })();                               
+    //     await expect(result).to.eql(finalyResult);                  
+    // });
+    // it('should appears special window for searching by image after press button "Search by image"',async function(){                           
+    //     await driver.get('https://www.google.com/imghp?hl=en&ogbl');                                 
+    //     await driver.findElement(By.xpath("//*[@class='LM8x9c']")).click();
+    //     await (await driver).sleep(1000);        
+    //     let windowForSearchDisplayed = await driver.findElement(By.id("QDMvGf")).isDisplayed();                          
+    //     await driver.quit();                           
+    //     await expect(windowForSearchDisplayed).to.equal(true);                  
+    // });
     
     //New tests after lection webdriver Actions and Executors.
     //This tests may be not needed for real project, but this tests give a chance training Actions and Executors methods 
-    it('should text "images" is blue for main logo for img-search page',async function(){                           
+    it('should been text "images" is blue for main logo for img-search page',async function(){                           
         await driver.get('https://www.google.com/');                                 
         let button = await driver.findElement(By.linkText("Images"));                        
         await driver.actions().click(button).perform();                          
@@ -99,4 +99,15 @@ describe("Google.com unit testing", function(){
         await driver.quit();
         await expect(colorText).to.equal("rgba(66, 133, 244, 1)");                 
     });
-});    
+    it('should correctly displayed panel "content info" for search results page',async function(){                           
+        await driver.get('https://www.google.com/');                                 
+        let inputForText = await driver.findElement(By.name("q"));        
+        await driver.actions(inputForText).sendKeys("wikipedia",Key.ENTER).perform();
+        //the next step only to use the scroll method, I understand that this step is not needed for isDisplayed ()
+        await driver.executeScript('window.scrollTo(0, document.body.scrollHeight)');
+        let contentInfoDisplay = await driver.findElement(By.id("footcnt")).isDisplayed();
+        await driver.quit();
+        await expect(contentInfoDisplay).to.equal(true);              
+    });
+}); 
+   
