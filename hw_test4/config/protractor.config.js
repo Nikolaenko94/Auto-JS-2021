259b0fa83fdf6459fd76f834d996182348ac1631
Nikolaenko94 <grot_cs5@mail.ru>
2021-05-23 17:21:36 +0300

    feat: add tests for Main Search submodule into mainSearch.spec.js

--- hw_test4/config/protractor.config.js
+++ hw_test4/config/protractor.config.js
@@ -11,7 +11,7 @@ exports.config = {
     capabilities: {
         browserName: 'chrome',
         chromeOptions: {
-                args: ['--lang=es-ES']}       
+                args: ['--headless','--lang=en-EN']}       
     },
 
      baseUrl: 'https://google.com/',
