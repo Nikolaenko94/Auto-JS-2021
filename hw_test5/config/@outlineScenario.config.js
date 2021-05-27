637cd8401b62f44b293213b8dfc9e5313786a14a
Nikolaenko94 <grot_cs5@mail.ru>
2021-05-27 15:20:37 +0300

    feat: @outlineScenario can run with node js with global vatiable - TAGS_OUTLINESCENARIO. Bonus task

---
+++ hw_test5/config/@outlineScenario.config.js
@@ -0,0 +1,31 @@
+const {resolve} = require('path');
+const reportsDir = "../reports";
+
+exports.config = {   
+
+    directConnect: true,
+    framework: 'custom',
+    frameworkPath: require.resolve('protractor-cucumber-framework'),
+    specs:[resolve('./hw_test5/features/**/*.feature')],
+
+    allScriptsTimeout: 360000,
+    getPageTimeout: 360000,
+
+    capabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+                args: ['--headless','--lang=en-EN','--incognito']
+            },       
+    },
+
+    cucumberOpts: {
+        format: [`json:${reportsDir}/report.json`],
+        tags: ' @outlineScenario',
+    },
+
+    onPrepare: async function(){
+        await browser.waitForAngularEnabled(false);
+        await browser.manage().window().setSize(1366,768);
+    },
+
+};
