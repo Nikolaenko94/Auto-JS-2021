3c92c18adbe3a843bd7a9978958961fa8a7422ac
Nikolaenko94 <grot_cs5@mail.ru>
2021-05-31 12:58:39 +0300

    fix: fix test for ALBUMS:[GET] should return success status [200, 'OK']

--- hw_test6/test/specs/albums.spec.js
+++ hw_test6/test/specs/albums.spec.js
@@ -22,14 +22,13 @@ describe("[GET]ALBUMS", function(){
     afterEach(()=>{
         driver.quit()
     }) 
-    it('',async function(){                          
-        it('should return success status [200, "OK"]',async function(){
-            //arrange
-            const URL = 'https://jsonplaceholder.typicode.com/albums';
-            //act
-            const result = await axios.get(URL);
-            //assert                          
-            await expect([result.status,result.statusText]).to.eql([200, "OK"]);          
-        });                 
+    it('should return success status [200, "OK"]',async function(){
+        //arrange
+        const URL = 'https://jsonplaceholder.typicode.com/albums';
+        //act
+        const result = await axios.get(URL);
+        //assert                          
+        await expect([result.status,result.statusText]).to.eql([200, "OK"]);                           
+                        
     });
 });
