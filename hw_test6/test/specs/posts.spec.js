4e1d6823f0e35ef6f3113f62307a14dbdca55364
Nikolaenko94 <grot_cs5@mail.ru>
2021-05-31 11:48:25 +0300

    feat: add test for POSTS: 'should return success status [200, 'OK']

---
+++ hw_test6/test/specs/posts.spec.js
@@ -0,0 +1,33 @@
+const axios = require("axios").default;
+const chai = require("chai");
+const spies = require("chai-spies");
+const { expect } = require("chai");
+const { Executor } = require("selenium-webdriver/http");
+chai.use(spies);
+const webdriver = require("selenium-webdriver"),
+      By = webdriver.By,
+      until = webdriver.until,
+      Key = webdriver.Key,
+      chrome = require("selenium-webdriver/chrome"),
+      path = require("chromedriver").path;
+       
+describe("Posts API testing", function(){
+    let driver;
+    beforeEach(()=>{
+        driver = new webdriver.Builder()
+        .setChromeOptions(new chrome.Options().headless().addArguments('--lang=en-EN', '--window-size=1366,768'))
+        .withCapabilities(webdriver.Capabilities.chrome())        
+        .build();
+    });                 
+    afterEach(()=>{
+        driver.quit()
+    }) 
+    it('should return success status [200, "OK"]',async function(){
+        //arrange
+        const URL = 'https://jsonplaceholder.typicode.com/posts';
+        //act
+        const result = await axios.get(URL);
+        //assert                          
+        await expect([result.status,result.statusText]).to.eql([200, "OK"]);          
+    });
+});
---
+++ hw_test6/test/specs/users.spec.js
@@ -0,0 +1,31 @@
+const axios = require("axios").default;
+const chai = require("chai");
+const spies = require("chai-spies");
+const { expect } = require("chai");
+const { Executor } = require("selenium-webdriver/http");
+chai.use(spies);
+const webdriver = require("selenium-webdriver"),
+      By = webdriver.By,
+      until = webdriver.until,
+      Key = webdriver.Key,
+      chrome = require("selenium-webdriver/chrome"),
+      path = require("chromedriver").path;
+       
+describe("Posts API testing", function(){
+    let driver;
+    beforeEach(()=>{
+        driver = new webdriver.Builder()
+        .setChromeOptions(new chrome.Options().headless().addArguments('--lang=en-EN', '--window-size=1366,768'))
+        .withCapabilities(webdriver.Capabilities.chrome())        
+        .build();
+    });                 
+    afterEach(()=>{
+        driver.quit()
+    }) 
+    it('',async function(){                          
+        //arrange
+        //act
+        //assert                          
+        await expect().to.equal();                 
+    });
+});
