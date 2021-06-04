271fe1d6ee7a1522ff136dc6b52a1798059b549d
Nikolaenko94 <grot_cs5@mail.ru>
2021-06-05 01:16:01 +0300

    feat: add person Logger into config/logger.js

--- hw_test7/specs/compatibility.spec.js
+++ hw_test7/specs/compatibility.spec.js
@@ -1,8 +1,9 @@
 const {browser, element} = require("protractor");
 const BasePage = require("../pages/base-page");
+const Logger  = require("../config/logger") ;
 const  highilight = require('../config/hightlight');
+const moduleName = __filename.slice(__dirname.length + 1);
 let hgElement = null;
-
 describe("compatibility unit-testing", function(){
     afterEach(()=>{
         highilight.highilight(hgElement);
@@ -10,6 +11,11 @@ describe("compatibility unit-testing", function(){
     });
     it('should open www.google.com',async function(){
         await browser.get('https://www.google.com/');
+        await Logger.error(moduleName, "example for Logger Error");
+        await Logger.warn(moduleName, "example for Logger Warn");
+        await Logger.info(moduleName, "example for Logger Info");
+        await Logger.verbose(moduleName, "example for Logger Verbose");
+        await Logger.debug(moduleName, "example for Logger Debug");
         hgElement = await element(by.css("body"));
         const url = await BasePage.currentUrl();                 
         await expect(url).toBe('https://www.google.com/');             
